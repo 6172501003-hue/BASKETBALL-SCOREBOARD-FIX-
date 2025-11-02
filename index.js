83765cb75b81ff8a5935be26f04e327e411c060e
6172501003-hue <6172501003@student.unpar.ac.id>
2025-11-03 00:15:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
 const scoreHome = document.getElementById("scoreHome")
 const scoreGuest = document.getElementById("scoreGuest")
-let homeScore = 0;
-let guestScore = 0;
+let homeScore = 0; //variabel untuk menyimpan score Home
+let guestScore = 0; //variabel untuk menyimpang score Guess
 
-function addHome(points) {
+function addHome(points) { //menambah poin dari score Home
     homeScore += points;
     document.getElementById("homeScore").innerText = homeScore;
 }
 
-function minusHome(points) {
+function minusHome(points) { //mengurangi poin dari score Home
     homeScore -= points;
     document.getElementById("homeScore").innerText = homeScore
 }
 
-function resetHome(points) {
+function resetHome(points) { //mengatur ulang poin dari score Home
     homeScore = 0;
     document.getElementById("homeScore").innerText = homeScore
 }
 
-function addGuest(points) {
+function addGuest(points) { //menambah poin dari score Guess
     guestScore += points;
     document.getElementById("guestScore").innerText = guestScore
 }
 
-function minusGuest(points) {
+function minusGuest(points) { //mengurangi poin dari score Guess
     guestScore -= points;
     document.getElementById("guestScore").innerText = guestScore
 }
 
-function resetGuest(points) {
+function resetGuest(points) { //mengatur ulang poin dari score Guess
     guestScore = 0;
     document.getElementById("guestScore").innerText = guestScore
 }
 
 let Timer;
-let timeLeft = 10 * 60;
+let timeLeft = 10 * 60; //menetapkan waktu hitung mundur dari 10 menit 60 detik.
 
-function startTimer() {
+function startTimer() { //untuk memulai, menghentikan, dan mengatur ulang timer.
   clearInterval(Timer); // prevent multiple timers
     Timer = setInterval(() => {
     if (timeLeft <= 0) {
@@ -44,20 +44,20 @@ function startTimer() {
         document.getElementById("timer").innerText = "00:00";
     } else {
         let minutes = Math.floor(timeLeft / 60);
-        let seconds = timeLeft % 60;
+        let seconds = timeLeft % 60; //Mengambil sisa hasil bagi. (Contoh: 599 % 60 adalah 59, karena $599 = 9 \times 60 + 59$).
         document.getElementById("timer").innerText =
         `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
         timeLeft--;
     }
-    }, 1000);
-}
+    }, 1000); //Interval waktu dalam milidetik (1000 ms = 1 detik). Jadi, kode di dalamnya akan berjalan setiap 1 detik.
+} 
 
-function stopTimer() {
+function stopTimer() { //menjeda/menghentikan timer
     clearInterval(Timer);
 }
 
 function resetTimer() {
-    clearInterval(Timer);
+    clearInterval(Timer); //untuk mengatur ulang timer
     timeLeft = 10 * 60;
     document.getElementById("timer").innerText = "10:00";
 }
